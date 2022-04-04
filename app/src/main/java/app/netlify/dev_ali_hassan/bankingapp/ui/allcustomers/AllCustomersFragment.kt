2d7411fb28d0bc084f685c320df50bf7b6836718
Ali Hassan <alihassan17122002@gmail.com>
2022-04-04 09:03:50 -0800

    added the first screen in the application navigation diagram.

--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersFragment.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersFragment.kt
@@ -1,4 +1,23 @@
 package app.netlify.dev_ali_hassan.bankingapp.ui.allcustomers
 
-class AllCustomersFragment {
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.Fragment
+import app.netlify.dev_ali_hassan.bankingapp.R
+
+const val TAG = "AllCustomersFragment"
+/**
+ *
+ * The Fragment will be display all customer in a list back-ended be the AllCustomersViewModel
+ */
+class AllCustomersFragment : Fragment(R.layout.all_customers_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d(TAG, "onViewCreated: starts")
+        // will be filled in the up coming commits
+
+    }
 }
--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersViewModel.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersViewModel.kt
@@ -1,4 +1,10 @@
 package app.netlify.dev_ali_hassan.bankingapp.ui.allcustomers
 
-class AllCustomersViewModel {
+import androidx.lifecycle.ViewModel
+
+/**
+ * This view model will be responsible for the business logic of the all customers fragment. I will interact
+ * with the database, and take all the back end side.
+ */
+class AllCustomersViewModel: ViewModel() {
 }
