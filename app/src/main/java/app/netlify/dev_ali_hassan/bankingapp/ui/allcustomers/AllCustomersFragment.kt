32d1f08ebe8f2405e4eb1acd34f12e066a3d1a53
Ali Hassan <alihassan17122002@gmail.com>
2022-04-06 11:42:21 -0800

    test the app with fake data

--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersFragment.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersFragment.kt
@@ -3,9 +3,16 @@ package app.netlify.dev_ali_hassan.bankingapp.ui.allcustomers
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import app.netlify.dev_ali_hassan.bankingapp.R
+import app.netlify.dev_ali_hassan.bankingapp.data.models.Customer
+import app.netlify.dev_ali_hassan.bankingapp.databinding.AllCustomersFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 
 
 /**
@@ -13,17 +20,46 @@ import dagger.hilt.android.AndroidEntryPoint
  * The Fragment will be display all customer in a list back-ended be the AllCustomersViewModel
  */
 @AndroidEntryPoint
-class AllCustomersFragment : Fragment(R.layout.all_customers_fragment) {
+class AllCustomersFragment : Fragment(R.layout.all_customers_fragment), AllCustomerIsAdapter.OnCustomerSelected {
+
+    private lateinit var binding: AllCustomersFragmentBinding
+    private val viewModel: AllCustomersViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val adapter = AllCustomerIsAdapter(this, requireContext())
+        binding.allCustoemrsRv.adapter = adapter
+        adapter.submitList(viewModel.provideTempData())
         Log.d(TAG, "onViewCreated: starts")
-        // will be filled in the up coming commits
 
+        // listen to events
+        listenToEvents()
     }
+
+
+    private fun listenToEvents() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.eventsFlow.collect {events ->
+                when (events) {
+                    is AllCustomersViewModel.AllCustomerEvents.NavigateToDetailsFragment -> {
+                        navigateToDetailsFragment(events.customer)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun navigateToDetailsFragment(customer: Customer) {
+        val data = bundleOf("selected_customer" to customer)
+        findNavController().navigate(R.id.action_allCustomersFragment_to_customerDetailsFragment, data)
+    }
+
 
     companion object {
         const val TAG = "AllCustomersFragment"
+    }
+
+    override fun onCustomerSelected(customer: Customer) {
+        viewModel.userSelectCustomer(customer)
     }
 }
--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersViewModel.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/allcustomers/AllCustomersViewModel.kt
@@ -1,7 +1,13 @@
 package app.netlify.dev_ali_hassan.bankingapp.ui.allcustomers
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.netlify.dev_ali_hassan.bankingapp.data.models.Customer
+import app.netlify.dev_ali_hassan.bankingapp.data.models.Gender
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 /**
@@ -9,5 +15,35 @@ import javax.inject.Inject
  * with the database, and take all the back end side.
  */
 @HiltViewModel
-class AllCustomersViewModel @Inject constructor(): ViewModel() {
+class AllCustomersViewModel @Inject constructor() : ViewModel() {
+
+
+    private val eventsChannel = Channel<AllCustomerEvents>()
+    val eventsFlow = eventsChannel.receiveAsFlow()
+
+
+    fun userSelectCustomer(customer: Customer) =
+        viewModelScope.launch {
+            eventsChannel.send(AllCustomerEvents.NavigateToDetailsFragment(customer))
+        }
+
+
+    sealed class AllCustomerEvents {
+        data class NavigateToDetailsFragment(val customer: Customer): AllCustomerEvents()
+    }
+
+
+
+    fun provideTempData() = listOf(
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE),
+        Customer("Ali Hassan", "38KDF938IOA", 600, "ali@gmail.com", 3, Gender.MALE)
+    )
 }
