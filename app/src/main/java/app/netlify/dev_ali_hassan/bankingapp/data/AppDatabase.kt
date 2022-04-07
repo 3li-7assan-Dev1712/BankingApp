e0f9ddd8abaa920a667f519278944445dba20914
Ali Hassan <alihassan17122002@gmail.com>
2022-04-07 10:17:18 -0800

    created the application database.

---
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/data/AppDatabase.kt
@@ -0,0 +1,14 @@
+package app.netlify.dev_ali_hassan.bankingapp.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import app.netlify.dev_ali_hassan.bankingapp.data.daos.CustomerDao
+import app.netlify.dev_ali_hassan.bankingapp.data.daos.TransformationsDao
+import app.netlify.dev_ali_hassan.bankingapp.data.models.Customer
+import app.netlify.dev_ali_hassan.bankingapp.data.models.Transformation
+
+@Database(entities = [Customer::class, Transformation::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun customersDao(): CustomerDao
+    abstract fun transformationsDao(): TransformationsDao
+}
--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/di/AppModel.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/di/AppModel.kt
@@ -1,4 +1,31 @@
 package app.netlify.dev_ali_hassan.bankingapp.di
 
+import android.app.Application
+import androidx.room.Room
+import app.netlify.dev_ali_hassan.bankingapp.data.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModel {
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(app: Application) =
+        Room.databaseBuilder(app, AppDatabase::class.java, "bank_database")
+            .createFromAsset("database/bank_database.db")
+            .build()
+
+    @Provides
+    fun provideCustomersDao(appDatabase: AppDatabase) =
+        appDatabase.customersDao()
+
+    @Provides
+    fun provideTransformationsDao(appDatabase: AppDatabase) =
+        appDatabase.transformationsDao()
 }
