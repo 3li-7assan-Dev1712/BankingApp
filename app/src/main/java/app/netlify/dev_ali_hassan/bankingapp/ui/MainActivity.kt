1f62bd34eb09a7ac4cbeb76302512115385bb16c
Ali Hassan <alihassan17122002@gmail.com>
2022-04-11 21:21:59 -0800

    added the datastore library to store light weight data (dark theme settings)

--- app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/MainActivity.kt
+++ app/src/main/java/app/netlify/dev_ali_hassan/bankingapp/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package app.netlify.dev_ali_hassan.bankingapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -19,14 +20,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
                 as NavHostFragment
         navController = navHostFragment.findNavController()
         setupActionBarWithNavController(navController)
+        navController.restoreState(savedInstanceState)
     }
 
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+
     }
 }
